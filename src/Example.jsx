6b0b34829c45a2ef0fc754236fe4c84447b4d729
Raveendra Kotari <raveendra040@gmail.com>
2023-05-07 00:59:31 +0530

    push karo

---
+++ src/Example.jsx
@@ -0,0 +1,17 @@
+import { CheckIcon } from '@heroicons/react/24/solid'
+
+const includedFeatures = [
+  'Private forum access',
+  'Member resources',
+  'Entry to annual conference',
+  'Official member t-shirt',
+]
+
+
+export default function Example() {
+  return (
+    <div>
+      Hello 
+    </div>
+  )
+}
